22eaf3119e5864663c413262b6017fcf580455bd
obinnanprogrammed <obinnanwakwue10@gmail.com>
2024-04-01 21:02:57 -0500

    I thought I dealt with these

--- backend/src/main/java/com/npcroom/NPCRoomCrackheadController.java
+++ backend/src/main/java/com/npcroom/NPCRoomCrackheadController.java
@@ -1,2 +1,13 @@
-package com.npcroom;public class NPCRoomCrackheadController {
+package com.npcroom;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+// add necessary beans up here (also don't worry if CORS doesn't work, we will deal with that later
+
+public class NPCRoomCrackheadController {
+    private String result;
+
+    public void processStatement(@RequestBody String message) {
+        // add processing logic (hint: it's pretty similar to the classic controller one. See NPCRoomController.java.
+    }
 }
